183c57dbe3a5dfdcc2ebbe12950ebab7a3aca75f
Alexis JEANNE <ajeanne@ippon.fr>
2022-07-21 18:29:59 +0200

    Création modules

---
+++ src/app/feature/accompanying/referee/referee.module.ts
@@ -0,0 +1,14 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+
+import { RefereeRoutingModule } from './referee-routing.module';
+
+
+@NgModule({
+  declarations: [],
+  imports: [
+    CommonModule,
+    RefereeRoutingModule
+  ]
+})
+export class RefereeModule { }
